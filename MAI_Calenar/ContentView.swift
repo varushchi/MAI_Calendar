2c01d14a4fba628b2dae9a808d832e1a6781e3b8
varushchi <vadim.shchigolev@mail.ru>
2026-02-12 17:24:38 +0300

    Initial Commit

---
+++ MAI_Calenar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MAI_Calenar
+//
+//  Created by Vadimaty Shchigol on 12.2.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MAI_Calenar/MAI_CalenarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAI_CalenarApp.swift
+//  MAI_Calenar
+//
+//  Created by Vadimaty Shchigol on 12.2.2026.
+//
+
+import SwiftUI
+
+@main
+struct MAI_CalenarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
